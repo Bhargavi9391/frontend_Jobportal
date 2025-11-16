488f804df55c817e4372ff4436b6b8647ca4c091
jaddu Bhargavi <157037629+Bhargavi9391@users.noreply.github.com>
2025-11-16 08:34:28 +0530

    Update Login.js

--- src/Login.js
+++ src/Login.js
@@ -21,11 +21,7 @@ function Login() {
   const { darkMode, toggleTheme } = useTheme();
   const [userCount, setUserCount] = useState(0);
   const navigate = useNavigate();
-const API_BASE = "https://backend-jobportal-xoym.onrender.com";
-
-
-
-
+  const API_BASE = "https://jobportal-backend-xoym.onrender.com";
 
   const conditions = [
     { regex: /[A-Z]/, text: "One uppercase letter" },
