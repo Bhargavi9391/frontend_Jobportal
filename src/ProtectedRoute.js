c3b5f874e3a4c33335436aa017f09d4b8174b63b
jaddu Bhargavi <157037629+Bhargavi9391@users.noreply.github.com>
2025-11-16 12:12:23 +0530

    Update ProtectedRoute.js

--- src/ProtectedRoute.js
+++ src/ProtectedRoute.js
@@ -6,13 +6,15 @@ export default function ProtectedRoute({ children, role }) {
   const user = JSON.parse(localStorage.getItem("user"));
 
   if (!token || !user) {
-    // Not logged in
+    // Not logged in at all
     return <Navigate to="/login" replace />;
   }
 
   if (role && user.role !== role) {
-    // Logged in but wrong role
-    return <Navigate to="/login" replace />;
+    // Logged in but role mismatch
+    // Redirect normal users to /home, admin to /admin
+    if (user.role === "admin") return <Navigate to="/admin" replace />;
+    else return <Navigate to="/home" replace />;
   }
 
   return children;
